b611d03d5319eaa61bc79f1f4a56cbf858f6eec6
Ari <ari.abramowitz1@gmail.com>
2019-03-25 21:13:13 -0400

    assignment 2965 testing: completed

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -1,11 +1,12 @@
 const MenuController = require("../controllers/MenuController");
 
 describe("MenuController", () => {
-  describe("#getContactCount", () => {
 
-    beforeEach(() => {
-      this.menu = new MenuController();
-    });
+  beforeEach(() => {
+    this.menu = new MenuController();
+  });
+
+  describe("#getContactCount", () => {
 
     it("should return 0 when no contacts are in the book", () => {
       expect(this.menu.getContactCount()).toBe(0);
@@ -13,6 +14,12 @@ describe("MenuController", () => {
     it("should return 1 when theris exactly one contact in the book", () => {
       this.menu.contacts.push("Bob");
       expect(this.menu.getContactCount()).toBe(1);
-    })
+    });
+  });
+
+  describe('#remindMe', () => {
+    it('should return the string: Learning is a life-long pursuit.', () => {
+      expect(this.menu.remindMe()).toBe('Learning is a life-long pursuit.');
+    });
   });
 });
