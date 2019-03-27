1f1b0e2bda60b087cd2d3903a228a980d76b10a6
Ari <ari.abramowitz1@gmail.com>
2019-03-27 15:36:21 -0400

    checkpoint 2964 creating contacts and connecting to a database: completed

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -6,17 +6,6 @@ describe("MenuController", () => {
     this.menu = new MenuController();
   });
 
-  describe("#getContactCount", () => {
-
-    it("should return 0 when no contacts are in the book", () => {
-      expect(this.menu.getContactCount()).toBe(0);
-    });
-    it("should return 1 when theris exactly one contact in the book", () => {
-      this.menu.book.addContact("Bob", "555-555-5555");
-      expect(this.menu.getContactCount()).toBe(1);
-    });
-  });
-
   describe('#remindMe', () => {
     it('should return the string: Learning is a life-long pursuit.', () => {
       expect(this.menu.remindMe()).toBe('Learning is a life-long pursuit.');
